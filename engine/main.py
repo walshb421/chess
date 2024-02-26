82c15ae673d303492b49868edfa16d50d7128ee8
GarettOS <garett2777@gmail.com>
2024-02-25 23:42:33 -0800

    added basic game loop, created pieces class and made pawns you can move with chess notation

--- engine/main.py
+++ engine/main.py
@@ -3,7 +3,29 @@ from board import ChessBoard
 def main():
     board = ChessBoard()
     board.create_starting_board()
-    board.print_board()
+    
+    game_end = False
+    while(game_end == False):
+        board.print_board()
+        print('''What will you do?
+            1 - Move
+            2 - End game''')
+        
+        user_choice = input("Enter your choice: ")
+        
+        if (user_choice == "1"):
+            print("Enter square you want moved (ex. e4, d6, h2)")
+            start_pos = input()
+            print("Enter square you want to move to (ex. e4, d6, h2)")
+            end_pos = input()
+            board.move_piece(start_pos, end_pos)
+
+        elif (user_choice == "2"):
+            print("l0ser")
+            game_end = True
+
+        print("-------")
+        
 
 if __name__ == "__main__":
     main()
