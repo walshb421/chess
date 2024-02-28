ea85d95a290944a1a8efbb97111bef618d812838
Benjiman Walsh <walsh.ben@hotmail.com>
2024-02-27 22:09:15 -0800

    Add simple hello world websocket

--- Dockerfile
+++ Dockerfile
@@ -4,7 +4,7 @@ WORKDIR /usr/src/app
 
 COPY package*.json ./
 
-RUN npm install
+RUN npm ci
 
 COPY vite.config.js .
 COPY index.html .
--- src/App.vue
+++ src/App.vue
@@ -1,7 +1,13 @@
 <script setup>
+import { useWebSocket } from '@vueuse/core'
+
+const { status, data, send, open, close } = useWebSocket('ws://localhost:9090')
 
 </script>
 
 <template>
   <h1>Example Chess App</h1>
+  <p>Status: {{ status }}</p>
+  <p>Data: {{ data }}</p>
+  <button @click="send('hello world')">Hello World</button>
 </template>
