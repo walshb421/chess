4138914b9de2bb4e0aa99779c4bc4604fcc660d2
Benjiman Walsh <walsh.ben@hotmail.com>
2024-02-28 18:41:08 -0800

    Added watchdog to restart websocket server on change

--- engine/app.py
+++ engine/app.py
@@ -1,1 +1,8 @@
-print("Hello World")
+import asyncio
+from websocket import server
+
+async def main(websocket):
+    async for message in websocket:
+        await websocket.send("message")
+
+asyncio.run(server(main))
--- engine/websocket.py
+++ engine/websocket.py
@@ -3,12 +3,6 @@
 import asyncio
 from websockets.server import serve
 
-async def echo(websocket):
-    async for message in websocket:
-        await websocket.send(message)
-
-async def main():
-    async with serve(echo, "0.0.0.0", 8765):
+async def server(callback):
+    async with serve(callback, "0.0.0.0", 8765):
         await asyncio.Future()  # run forever
-
-asyncio.run(main())
