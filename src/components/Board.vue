f4863fda98e18960e34df9a2c390b9ea7cca24a7
Benjiman Walsh <walsh.ben@hotmail.com>
2024-03-01 10:36:21 -0800

    Added a solid board design

--- src/components/Board.vue
+++ src/components/Board.vue
@@ -1,17 +1,30 @@
 <script setup>
 
-const board = new Array(8).fill(
-    new Array(8).fill(null) 
-);
-
-console.log(board);
+const horizontal = ["A", "B", "C", "D", "E", "F", "G", "H"];
+const vertical = ["8", "7", "6", "5", "4", "3", "2", "1"];
 
 </script>
 <template>
     <div class="board">
-        <div v-for="row in board" class="board-row">
-            <div v-for="square in row" class="board-square">
-                
+        <div class="board-row">
+            <div v-for="letter in horizontal" class="board-column-label">
+                <p>{{ letter }}</p>
+            </div>
+        </div>
+        <div v-for="num in vertical" class="board-row">
+            <div class="board-row-label">
+                <p>{{ num }}</p>
+            </div>
+            <div v-for="letter in horizontal" class="board-square">
+                <p class="board-square-label">{{ letter + num }}</p>
+            </div>
+            <div class="board-row-label">
+                <p>{{ num }}</p>
+            </div>
+        </div>
+        <div class="board-row">
+            <div v-for="letter in horizontal" class="board-column-label">
+                <p>{{ letter }}</p>
             </div>
         </div>
     </div>
