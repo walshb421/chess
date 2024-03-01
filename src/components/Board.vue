d5a27e7424db0bd0c043e487d4cac31513734b3c
Benjiman Walsh <walsh.ben@hotmail.com>
2024-03-01 11:06:09 -0800

    Added comments to board code

--- src/components/Board.vue
+++ src/components/Board.vue
@@ -6,22 +6,36 @@ const vertical = ["8", "7", "6", "5", "4", "3", "2", "1"];
 </script>
 <template>
     <div class="board">
+
+        <!-- Column Labels (Letters)-->
         <div class="board-row">
             <div v-for="letter in horizontal" class="board-column-label">
                 <p>{{ letter }}</p>
             </div>
         </div>
+
         <div v-for="num in vertical" class="board-row">
+
+            <!-- Row Labels (Numbers)-->
             <div class="board-row-label">
                 <p>{{ num }}</p>
             </div>
+
+            <!-- Core Squares of the Board -->
             <div v-for="letter in horizontal" class="board-square">
+
+                <!-- Logic For Pieces -->
                 <p class="board-square-label">{{ letter + num }}</p>
+                
             </div>
+
+            <!-- Row Labels (Numbers)-->
             <div class="board-row-label">
                 <p>{{ num }}</p>
             </div>
         </div>
+
+        <!-- Column Labels (Letters)-->
         <div class="board-row">
             <div v-for="letter in horizontal" class="board-column-label">
                 <p>{{ letter }}</p>
